9e0179ff636ec9d0edceb9b11b7252ed20086741
willis <dsa663838@gmail.com>
2023-08-11 22:10:49 +0100

    Format

--- src/handlers/authorizer.ts
+++ src/handlers/authorizer.ts
@@ -1,6 +1,4 @@
-// invoke locally, run
-// sam build Authorizer && sam local invoke Authorizer -e src/authorizer/events/event.json
-import { APIGatewayTokenAuthorizerHandler, APIGatewayAuthorizerResult, PolicyDocument, APIGatewayAuthorizerResultContext } from 'aws-lambda';
+import { APIGatewayTokenAuthorizerHandler, APIGatewayAuthorizerResult } from 'aws-lambda';
 
 export const lambdaHandler: APIGatewayTokenAuthorizerHandler = async (event, context) => {
   return generateAdminPolicy();
